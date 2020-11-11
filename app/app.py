8b67def8f18994e2273a67bf9a3c167223e95a31
manhlab <manhtranduc1111@gmail.com>
2020-11-11 17:55:32 +0300

    update database + html

--- app/app.py
+++ app/app.py
@@ -14,9 +14,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, ForeignKey, Integer, String, Date, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 from utils import time_format, hash_format
 import os
 import sys
@@ -24,11 +22,47 @@ import sys
 # SQL part
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.sqlite"
+# binds multiple database definition
+app.config['SQLALCHEMY_BINDS'] = {
+    "rooms": "sqlite:///rooms.sqlite",
+    "request": "sqlite:///request.sqlite"
+}
+
 app.config["SECRET_KEY"] = "random string"
 CORS(app)
 db = SQLAlchemy(app)
 
+# create multiple sqlalchemy engines
 
+class Rooms(db.Model):
+    __tablename__ = "rooms"
+    __bind_key__ = "rooms"
+    id = db.Column("room_id", db.Integer, primary_key=True)
+    room = Column(db.Integer, nullable=False)
+    number_person = Column(db.Integer, nullable=False)
+    type_room = Column(String(250), nullable=False)
+    price_room = Column(db.Integer, nullable=False)
+    position_room = Column(String(250), nullable=False)
+    status_room = Column(String(250), nullable=False)
+    
+
+    def __repr__(self):
+        return "<Room %r>" % self.room
+
+class Request(db.Model):
+    __tablename__ = "request"
+    __bind_key__ = "request"
+    id = db.Column("request_id", db.Integer, primary_key=True)
+    room = Column(db.Integer, nullable=False)
+    number_person = Column(db.Integer, nullable=False)
+    type_room = Column(String(250), nullable=False)
+    price_room = Column(db.Integer, nullable=False)
+    position_room = Column(String(250), nullable=False)
+    status_room = Column(String(250), nullable=False)
+    
+
+    def __repr__(self):
+        return "<Request %r>" % self.room
 
 
 
@@ -77,9 +111,7 @@ def __init__(
     self.endofcontract = endofcontract
     self.passwd = passport
 
-
 db.create_all()
-
 # Flask part
 @app.route("/", methods=["GET", "POST"])
 def request_form():
